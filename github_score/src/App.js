f35f82bec1fda102642473f46889477707c40994
Vince Carini <vincecarini@northwesternmutual.com>
2018-01-26 16:15:16 -0600

    score displaying. now for the conditional color and comment

--- github_score/src/App.js
+++ github_score/src/App.js
@@ -2,8 +2,49 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      returnedScore: null
+    }
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.getGitHubScore = this.getGitHubScore.bind(this);
+  }
+
+  handleUserNameChange(event) {
+    this.setState({username: event.target.value})
+  }
+
+  getGitHubScore (event) {
+    const promise = axios.get("https://api.github.com/users/" + this.state.username);
+    
+    promise.then( data => {
+      this.setState({returnedScore: data.data.public_repos + data.data.followers})
+      }, () => {}
+    )
+    
+    promise.catch( data => {
+      this.setState({returnedScore: "NotFound"})
+      }
+    )
+  }
+
   render() {
+    let scoreDisplay = <div className="scoreDisplay"></div>
+    let scoreComment = <div className="scoreComment"></div>
+    if (this.state.returnedScore === 0 || this.state.returnedScore) {
+      if (this.state.returnedScore === "NotFound") {
+        scoreDisplay = <div className="scoreDisplay">Not Found</div>
+      } else {
+        scoreDisplay = <div className="scoreDisplay">Your Score: {this.state.returnedScore}</div>
+        scoreComment = <div className="scoreComment">Great Job!</div>
+      }
+    }
+
     return (
       <div className="app">
         <div className="appBox">
@@ -15,22 +56,18 @@ class App extends Component {
               <div className="userInputLabel">
                 GitHub Username:
               </div>
-              <input type='text' className="userInput" placeholder="Enter a GitHub Username" >
+              <input type='text' className="userInput" placeholder="Enter a GitHub Username" onChange={this.handleUserNameChange} >
               </input>
             </div>
             <div className="buttonContainer">
-              <button className="calculateScoreButton">
+              <button className="calculateScoreButton" onClick={this.getGitHubScore} >
                 Calculate my GitHub Score
               </button>
             </div>
           </div>
           <div className="rightSide">
-            <div className="scoreDisplay">
-              Your Score: 135
-            </div>
-            <div className="scoreComment">
-              Great Job!
-            </div>
+            {scoreDisplay}
+            {scoreComment}
           </div>
         </div>
       </div>
